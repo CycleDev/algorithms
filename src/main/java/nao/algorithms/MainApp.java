eabd0538c24b2f28b750d22e24fce19087b8c532
noprysk <noprysk@n-ix.com>
2017-04-20 21:51:18 +0300

    Create app

---
+++ src/main/java/nao/algorithms/MainApp.java
@@ -0,0 +1,8 @@
+package nao.algorithms;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("App started!!!");
+    }
+}
---
+++ src/test/java/nao/algorithms/FirstTest.java
@@ -0,0 +1,12 @@
+package nao.algorithms;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void myFirstTest() {
+        Assert.assertEquals(2, 1 + 1);
+    }
+}
